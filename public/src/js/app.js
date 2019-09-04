bb9f673ef5eaa4c2b7b6cd1dbfc879cffedaffc3
arjunkesava <arjun.kesava@gmail.com>
2019-09-04 20:18:29 +0530

    Added cache support for Offline Internet with Service Worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,16 +1,24 @@
+
 var deferredPrompt;
 
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('/sw.js')
-		.then(function () {
-			console.info('serviceWorker register done!!!');
-		});
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-window.addEventListener('beforeinstallprompt', function (event) {
-	console.log('beforeinstallprompt fired');
-	event.preventDefault();
-	deferredPrompt = event;
-	return false;
-})
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
